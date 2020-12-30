1b4d8b678987a46d9be581a13ba717c856b65f8a
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2020-12-30 17:11:21 +0530

    password hashed in DB

--- WebDev/auth-jwt/models/User.js
+++ WebDev/auth-jwt/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -15,6 +16,17 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Please enter an password!'],
         minlength: [4, 'Please enter a password that is >= 4 characters']
     }
+
+});
+
+
+// firing a function before doc is saved to hash passwords
+userSchema.pre('save', async function(next) {
+    
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt);
+
+    next();
 
 });
 
