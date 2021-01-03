8a94d234cfa67ea503491881002c100f7c01be20
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2021-01-03 16:05:43 +0530

    JWT tokens added

--- WebDev/auth-jwt/app.js
+++ WebDev/auth-jwt/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/authRoutes');
 
@@ -8,6 +9,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -23,9 +25,3 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
-//cookies
-app.get('/set-cookies', (req,res) => {
-
-  res.setHeader('Set-Cookie','newUser=true');
-
-});
--- WebDev/auth-jwt/controllers/authController.js
+++ WebDev/auth-jwt/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 // Error handling
 const handleErrors = (err) => {
@@ -25,6 +26,18 @@ const handleErrors = (err) => {
     return errors;
 };
 
+const maxAge = 3*24*60*60; // 3 days in secs
+
+const createToken =  (id) => {
+
+    return jwt.sign({id}, 'mazhar secret code', { 
+        
+        expiresIn: maxAge 
+    
+    });
+
+};
+
 module.exports.signup_get = (req,res) => {
 
     res.render('signup');
@@ -45,8 +58,11 @@ module.exports.signup_post = async (req,res) => {
         
         const user = await User.create({ email, password});
 
-        res.status(201).json(user);
+        const token = createToken(user._id);
 
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000 });
+
+        res.status(201).json({ user: user._id });
 
     } catch (err) {
         
